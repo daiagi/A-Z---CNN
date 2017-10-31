54c553d663735039c4daf6635e33ca3ab07ace89
daiagi <daiagi@gmail.com>
2017-10-31 16:01:01 +0200

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -61,9 +61,10 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
-
+print('strating fit!')
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
                          epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 2000,
+                        n_jobs = -1)
